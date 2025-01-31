cf5a05b31412b41f4596ce87a4ed23c58fd46311
Orkhan <aliyevorhan82@gmail.com>
2025-01-31 22:06:19 +0400

    add api and config file and section

---
+++ MinioApi/Config/RemoteFileConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace MinioApi.Config
+{
+    public sealed class RemoteFileConfig
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; }= string.Empty;
+        public string Host { get; set; } = string.Empty;
+        public string Port { get; set; } = string.Empty;
+        public string BucketName { get; set; } = string.Empty;
+    }
+}
